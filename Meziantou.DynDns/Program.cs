4ad37c9fb82ef1c37d6e217595fdf00ea9570982
Gérald Barré <git@meziantou.net>
2023-10-21 15:56:38 -0400

    revert http3

--- Meziantou.DynDns/Program.cs
+++ Meziantou.DynDns/Program.cs
@@ -2,15 +2,6 @@
 using Microsoft.AspNetCore.Server.Kestrel.Core;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.WebHost.ConfigureKestrel((context, options) =>
-{
-    options.ListenAnyIP(8080, listenOptions =>
-    {
-        listenOptions.Protocols = HttpProtocols.Http1AndHttp2AndHttp3;
-        listenOptions.UseHttps();
-    });
-});
-
 builder.Services.Configure<DynDnsConfiguration>(builder.Configuration.GetSection("DynDns"));
 builder.Services.Configure<CloudflareConfiguration>(builder.Configuration.GetSection("Cloudflare"));
 builder.Services.AddSingleton<DnsUpdater, CloudflareDnsUpdater>();
